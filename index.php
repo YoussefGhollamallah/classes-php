dd2983413d0c08b0e3c7490dcd9ab8f219790bda
YoussefGhollamallah <youssef.gh@live.fr>
2024-08-26 13:20:37 +0200

    import de la page d'inscription

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+</head>
+<body>
+
+    <h1>Bienvenue sur la page d'inscription</h1>
+
+    <?php include_once "pages/inscription.php"; ?>
+
+</body>
+</html>
