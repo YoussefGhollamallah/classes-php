a59e26ee7a48b4fa82d68fc9646a580a3da258d8
YoussefGhollamallah <youssef.gh@live.fr>
2024-08-26 11:20:14 +0200

    first commit : création de la class User

---
+++ User.php
@@ -0,0 +1,20 @@
+<?php
+
+class User {
+
+    private int $id;
+    public string $login;
+    private string $password;
+    public string $email;
+    public string $firstname;
+    public string $lastname;
+
+    public function __construct($login,)
+    {
+        
+    }
+
+
+}
+
+?>
