46340c2d889680f91ec9c5a4b3f68273cf4a3f6e
YoussefGhollamallah <youssef.gh@live.fr>
2024-08-27 16:10:11 +0200

    nouvelle mise en place des fichiers pages

--- pages/inscription.php
+++ pages/inscription.php
@@ -4,35 +4,45 @@ require_once (__DIR__. "/../includes/_header.php");
 
 ?>
 
+<main>
 
-<section id="form-inscription">
-    
+    <section id="form-inscription">
+        
     <form  action="traitement_inscription.php" method="post">
-        <label for="login">Login :</label>
-        <input type="text" id="login" name="login" required><br><br>
-        
-        <label for="email">Email :</label>
-        <input type="email" id="email" name="email" required><br><br>
-        
-        <label for="firstname">Prénom :</label>
-        <input type="text" id="firstname" name="firstname" required><br><br>
-        
-        <label for="lastname">Nom :</label>
-        <input type="text" id="lastname" name="lastname" required><br><br>
-        
-        <label for="password">Mot de passe :</label>
-        <input type="password" id="password" name="password" required><br><br>
-        
-        <label for="confirm_password">Confirmez le mot de passe :</label>
-        <input type="password" id="confirm_password" name="confirm_password" required><br><br>
-        
-        <input type="submit" value="S'inscrire">
-    </form>
+    <label for="login">Login :</label>
+    <input type="text" id="login" name="login" required><br><br>
     
+    <label for="email">Email :</label>
+    <input type="email" id="email" name="email" required><br><br>
+
+    <div class="name-container">
+        <div class="name-field">
+            <label for="firstname">Prénom :</label>
+            <input type="text" id="firstname" name="firstname" required>
+        </div>
+
+        <div class="name-field">
+            <label for="lastname">Nom :</label>
+            <input type="text" id="lastname" name="lastname" required>
+        </div>
+    </div>
+    
+    <br><br>
+    
+    <label for="password">Mot de passe :</label>
+    <input type="password" id="password" name="password" required><br><br>
+    
+    <label for="confirm_password">Confirmez le mot de passe :</label>
+    <input type="password" id="confirm_password" name="confirm_password" required><br><br>
+    
+    <input type="submit" value="S'inscrire">
+</form>
+
 </section>
-    
+
+</main>
     <?php 
 
-require_once (__DIR__ . "/../includes/_footer.php");
+    require_once (__DIR__ . "/../includes/_footer.php");
 
-?>
+    ?>
