3222ad0339a0980e0312914e263a3da2de51d75c
YoussefGhollamallah <youssef.gh@live.fr>
2024-08-27 16:49:16 +0200

    création d'une page 404

---
+++ pages/page404.php
@@ -0,0 +1,37 @@
+<style>
+    
+    .container{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100dvh;
+    }
+
+        h1 {
+            font-size: 100px;
+            font-weight: bold;
+            margin-bottom: 30px;
+            color: white;
+        }
+        p {
+            font-size: 24px;
+            margin-bottom: 20px;
+            color: white;
+        }
+        a {
+            color: #007bff;
+            text-decoration: none;
+            color: white;
+        }
+        a:hover {
+            text-decoration: underline;
+            color: white;
+        }
+    </style>
+
+<div class="container">
+    <h1>Error 404</h1>
+    <p>Oups ! La page que vous cherchez n'existe pas. </p>
+    <img src="assets/images/error404.svg" alt="Erreur 404" class="img-fluid" style="max-width: 400px; margin-top: 30px;">
+</div>
