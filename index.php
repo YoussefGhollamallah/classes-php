d8ada94d7ec5bee2f7ac03a23554018a1aee0720
YoussefGhollamallah <youssef.gh@live.fr>
2024-08-27 16:11:20 +0200

    mise en place d'une conditions d'appel des pages

--- index.php
+++ index.php
@@ -1,21 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css?<?php echo time(); ?>">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+   
 
+    <title>Document</title>
+</head>
+<body>
     <?php
 
       require_once (__DIR__ . "/includes/_header.php" ) ;
 
     ?>
+  <main>
+        <?php
+        // Définit la page par défaut
+        $page = isset($_GET['page']) ? $_GET['page'] : 'index';
 
-    <main>
+        // Chemin du fichier correspondant à la page
+        $file = __DIR__ . "/pages/" . $page . ".php";
 
-    <h1>Index</h1>
-
+        // Vérifie si le fichier existe, sinon, affiche une erreur
+        if (file_exists($file)) {
+            require_once($file);
+        } else {
+            echo "<h1>Bienvenue sur mon site</h1>";
+        }
+        ?>
     </main>
-
-
     <?php
 
     require_once ( __DIR__ . "/includes/_footer.php")
     ?>
-   
+   <script src="/assets/js/bootstrap.min.js"></script>
 </body>
 </html>
