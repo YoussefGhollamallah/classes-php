653b30807f83d0037937c449c510a30ca388954a
YoussefGhollamallah <youssef.gh@live.fr>
2024-08-27 10:01:44 +0200

    création de la classe User

---
+++ classes/User.php
@@ -0,0 +1,37 @@
+<?php 
+
+class User {
+
+    private int $id;
+    public string $login;
+    private string $password;
+    public string $email;
+    public string $firstname;
+    public string $lastname;
+
+
+    public function __construct($login, $password, $email, $firstname, $lastname)
+    {
+        $this->login = $login;
+        $this->setPassword($password);
+        $this->email = $email;
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
+    }
+
+        // Setter method for password to hash it before storing
+        public function setPassword(string $password): void
+        {
+            $this->password = password_hash($password, PASSWORD_DEFAULT);
+        }
+    
+        // Getter method for password (optional, usually not needed)
+        public function getPassword(): string
+        {
+            return $this->password;
+        }
+
+}
+
+
+?>
