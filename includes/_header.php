31e2fc4a019bf68f90451fb5c968169234f43ae8
YoussefGhollamallah <youssef.gh@live.fr>
2024-08-27 16:09:08 +0200

    nouvelle mise en page du header

--- includes/_header.php
+++ includes/_header.php
@@ -1,35 +1,23 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/style.css?<?php echo time(); ?>">
-    <link rel="stylesheet" href="assets/css/form.css?<?php echo time(); ?>">
-
-    <title>Document</title>
-</head>
-<body>
 <header>
 <nav class="navbar navbar-expand-lg navbar-light bg-primary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="../index.php ">Navbar</a>
+    <a class="navbar-brand" href="index.php">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav me-auto">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="index.php?index.php">Home</a>
         </li>
       </ul>
       
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="pages/inscription.php">Inscription</a>
+          <a class="nav-link" href="index.php?page=inscription">Inscription</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="pages/connexion.php">Cnnexion</a>
+          <a class="nav-link" href="index.php?page=connexion">Connexion</a>
         </li>
       </ul>
     </div>
