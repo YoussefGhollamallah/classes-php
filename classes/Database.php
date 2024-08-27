28e34e6a766626176b17ae0a5f306c8b61f12ca0
YoussefGhollamallah <youssef.gh@live.fr>
2024-08-27 10:16:10 +0200

    création de la classe permettant la connexion vers la base de donnée

---
+++ classes/Database.php
@@ -0,0 +1,38 @@
+<?php
+
+include_once "config.php";
+
+class Database {
+
+    private $host = "DM_NAME"; 
+    private $username = 'DM_USER'; 
+    private $password = 'DM_PASSWORD'; 
+    private $dbname = 'DM_HOST'; 
+    private $connection;
+
+    public function __construct() {
+        $this->connection = new mysqli($this->host, $this->username, $this->password, $this->dbname);
+
+        if ($this->connection->connect_error) {
+            die('Erreur de connexion : ' . $this->connection->connect_error);
+        }
+    }
+
+    public function query($sql) {
+        return $this->connection->query($sql);
+    }
+
+    public function prepare($sql) {
+        return $this->connection->prepare($sql);
+    }
+
+    public function getConnection() {
+        return $this->connection;
+    }
+
+    public function __destruct() {
+        $this->connection->close();
+    }
+}
+
+?>
