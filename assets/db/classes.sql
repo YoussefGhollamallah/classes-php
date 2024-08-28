746e2ecc8d565a70c20a20f77373c2bcdca7a83a
YoussefGhollamallah <youssef.gh@live.fr>
2024-08-28 14:09:29 +0200

    mise a jour de la base de données

--- assets/db/classes.sql
+++ assets/db/classes.sql
@@ -32,7 +32,7 @@ CREATE TABLE `utilisateurs` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `login` (`login`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,7 +41,7 @@ CREATE TABLE `utilisateurs` (
 
 LOCK TABLES `utilisateurs` WRITE;
 /*!40000 ALTER TABLE `utilisateurs` DISABLE KEYS */;
-INSERT INTO `utilisateurs` VALUES (1,'menbas','$2y$10$CZLS/ZRHYJn9kySyARoZz.fnJt9X6TiELMiWYCWwEappBlYINYrgy','youssef.gh@live.fr','Youssef','Ghollamallah'),(2,'allstar','$2y$10$Faju3oOJ/bdxv8LDArjZteFdC8NG16sXN4xFwvztUn4mGYTT/a7LC','ayman@laplateforme.io','ayman','Ghollamallah'),(3,'mikasa','$2y$10$Jg.jvBg4Cz6DF/WxAnreSudlusf3hkRXjtq/pVNdAUf2dtfsRBe42','mikasa@laplateforme.io','mikasa','sukasa'),(4,'alistar','$2y$10$6nMQ35hjFltTtSMVhAD1Ne7/Gu0.7sG/6YdfDHM.tMSCW44o8P986','alistar@live.fr','alistar','crowley'),(5,'test','$2y$10$Pw2Se5.ZeHMLOxsXf75hVOqZbos856eJ4oaCOy3T7ueB/m84Zgkzq','test@test.fr','test','test'),(6,'azz','$2y$10$lAHK0BZd.8gqZiGG2p6QWuLYnw4HIHIj9z6DtpSxRuILCSoLN7X/2','az@az.fr','youssef','ghollamallah'),(7,'magic','$2y$10$EiQ2RxJaQ0IiALT.rp1drOvq44ZC6NtSN5HFJtsEd3Shy1hPo00ru','magic@laplateforme.io','magic','magic'),(8,'pet','$2y$10$q1QOHDtAhXOBdNqbufbWXOk/RcpXLFQfWOKjzI6gGRGc6Nq1spI7S','azererbadjan@gmail.com','youssef','ghollamallah'),(9,'azze','$2y$10$LHWq/jBXXqNOG.EA8xMDmOpfTU0qIKCFP.eg1s1/O5W9w6ak67pE.','ay@gmail.fr','youssef','ghollamallah'),(10,'test2','$2y$10$K9w9E1VPeTjbb/9rAjp6J.cKAy0tyaO5VEM0DT9jpag1r3ODLlgB2','ameli@test.fr','abdelkader','ghollamallah');
+INSERT INTO `utilisateurs` VALUES (19,'youssef','$2y$10$lmK/QQ16nHyhJCxi7McHxe7GbmOeLhTvY5vGHfo5aCBiV6piqEzTC','youssef.gh@live.fr','Youssef','Ghollamallah'),(20,'nicolas','$2y$10$4PSb5OfsiolYbM60JYBGIO/zFeLM1bCxUdO6tl9dK8RtgjjdPkehS','nicolas.degabriel@laplateforme.io','nicolas','degabriel');
 /*!40000 ALTER TABLE `utilisateurs` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -54,4 +54,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-08-27 14:41:02
+-- Dump completed on 2024-08-28 14:08:45
