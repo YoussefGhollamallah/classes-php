a11fb683ed5b4cef64a62eff30f1911077e0a1c9
YoussefGhollamallah <youssef.gh@live.fr>
2024-08-29 11:54:55 +0200

    mise à jour : modification de l'appel de la méthode disconnect

--- pages/logout.php
+++ pages/logout.php
@@ -1,9 +1,19 @@
 <?php
-session_start(); // Démarrer la session
 
-// Détruire toutes les données de la session
-session_unset();
-session_destroy();
+// Inclure le fichier de la classe User
+require_once 'classes/User.php';
+
+// Connexion à la base de données
+$db = new mysqli('localhost', 'root', 'root', 'classes');
+
+// Vérifier la connexion
+if ($db->connect_error) {
+    die("Échec de la connexion : " . $db->connect_error);
+}
+
+// Instancier l'utilisateur avec la connexion
+$user = new User($db);
+$user->disconnect();
 
 // Rediriger vers la page de connexion ou d'accueil
 header('Location: index.php?page=index');
